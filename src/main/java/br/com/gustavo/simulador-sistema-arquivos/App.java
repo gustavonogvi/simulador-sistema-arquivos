ebcf1a0ac773005956c164f64ca1ee9f026cca59
gustavonogvi <gustavonoggvinhas@gmail.com>
2025-05-28 12:57:54 -0300

    Projeto Maven inicializado com estrutura simulador de arquivos

---
+++ src/main/java/br/com/gustavo/simulador-sistema-arquivos/App.java
@@ -0,0 +1,13 @@
+package br.com.gustavo.simulador-sistema-arquivos;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
